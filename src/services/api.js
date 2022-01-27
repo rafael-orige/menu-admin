fd922f32bd4c7a2f6fd9b01ae4d5d568eda92e83
Rafael <fael.vargas@hotmail.com>
2022-01-27 15:19:51 -0300

    Api now hosted with Heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
-let url = "http://localhost:8080/";
+let url = "https://rafael-orige-admin-api.herokuapp.com";
 
 export default {
-    getStats: async () => {
-        const res = await fetch(url);
-        const json = await res.json();
-        return json;
-    }
-}
+  getStats: async () => {
+    const res = await fetch(url);
+    const json = await res.json();
+    return json;
+  },
+};
